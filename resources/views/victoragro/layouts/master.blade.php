c98d0e16b1abbd8527517c00f881159858604d77
techstackbusinessbd <techstackbusinessbd@gmail.com>
2025-05-30 16:33:12 +0600

    first commit

---
+++ resources/views/victoragro/layouts/master.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+
+<html lang="en" class="layout-navbar-fixed layout-menu-fixed layout-compact" dir="ltr" data-skin="default"
+    data-assets-path="{{ asset('assets/') }}" data-template="vertical-menu-template-starter" data-bs-theme="light">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>@yield('title')</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.ico') }}" />
+
+    @include('victoragro.layouts.partials.style')
+    @stack('styles')
+</head>
+
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+            @include('victoragro.layouts.partials.sidebar')
+            <!-- / Menu -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+                <!-- Navbar -->
+
+                @include('victoragro.layouts.partials.header')
+
+                <!-- / Navbar -->
+
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    <!-- Content -->
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        @yield('content')
+                    </div>
+                    <!-- / Content -->
+
+                    <!-- Footer -->
+                    @include('victoragro.layouts.partials.footer')
+                    <!-- / Footer -->
+
+                    <div class="content-backdrop fade"></div>
+                </div>
+                <!-- Content wrapper -->
+            </div>
+            <!-- / Layout page -->
+        </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+
+        <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+        <div class="drag-target"></div>
+    </div>
+    <!-- / Layout wrapper -->
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/theme.js -->
+    @include('victoragro.layouts.partials.script')
+    @stack('scripts')
+</body>
+
+</html>
---
+++ resources/views/victoragro/pages/home.blade.php
@@ -0,0 +1,11 @@
+{{-- resources/views/pages/home.blade.php --}}
+@extends('victoragro.layouts.master')
+
+@section('title', 'Home')
+
+@section('content')
+
+<h1>Hello, world!</h1>
+<p>This is your Laravel Blade page with a sidebar and header layout.</p>
+
+@endsection
